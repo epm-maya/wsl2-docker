29acccd38e2605314a64b8f50381390d4d2069eb
Noshiro Nanai <epm.maya@gmail.com>
2019-12-20 15:17:23 +0900

    Add sha256

--- make-rootfs.sh
+++ make-rootfs.sh
@@ -5,3 +5,7 @@ sudo ./alpine-make-rootfs \
 	--packages 'curl openssl openssh-client iptables xz' \
 	--script-chroot \
 	rootfs.tar.gz -- content/alpine.sh
+
+sha256sum rootfs.tar.gz > rootfs.tar.gz.sha256
+
+cat rootfs.tar.gz.sha256
